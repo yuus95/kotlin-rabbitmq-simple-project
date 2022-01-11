42845fe5f0faee5632b43022695937ca92ecf0d3
yushin <kkad45@naver.com>
2022-01-11 21:03:21 +0900

    git init

---
+++ src/main/kotlin/com/example/simplekotlinrabbitmq/SimpleKotlinRabbitmqApplication.kt
@@ -0,0 +1,11 @@
+package com.example.simplekotlinrabbitmq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleKotlinRabbitmqApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleKotlinRabbitmqApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/simplekotlinrabbitmq/SimpleKotlinRabbitmqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.simplekotlinrabbitmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleKotlinRabbitmqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
